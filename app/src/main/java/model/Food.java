cf697892195218ddbc100ec19611e50b6119e3f1
falcom0 <f.ichwan@gmail.com>
2018-01-24 21:13:01 +0700

    test-after crash

---
+++ app/src/main/java/model/Food.java
@@ -0,0 +1,8 @@
+package model;
+
+/**
+ * Created by PSI_DEV_07 on 1/23/2018.
+ */
+
+public class Food {
+}
---
+++ app/src/main/java/model/Promo.java
@@ -0,0 +1,8 @@
+package model;
+
+/**
+ * Created by PSI_DEV_07 on 1/24/2018.
+ */
+
+public class Promo {
+}
